122749fd8fab2bbfd2cf3cb7ce80481b1e5c40f7
Soulbow3 <138614183+Soulbow3@users.noreply.github.com>
2024-06-28 17:40:42 -0500

    Update monopoly.js

--- monopoly/monopoly.js
+++ monopoly/monopoly.js
@@ -339,7 +339,7 @@ back.style.background="red";}}
 function piece(element) {
 if (element.innerHTML==="Two") {
 element.innerHTML="";}
-else if (element.innerHTML==="")
+else if (element.innerHTML==="") {
 element.innerHTML="One";}
 else if (element.innerHTML==="Two") {
 element.innerHTML="";}
