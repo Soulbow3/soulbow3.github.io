77cb9385a77ae3dabf1ac1e864d9ea5b9674e71f
Soulbow3 <138614183+Soulbow3@users.noreply.github.com>
2024-07-02 17:05:36 -0500

    Create new.js

---
+++ checkers/new.js
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<head><html lang="en"><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=Edge"><meta name="viewport" content="width=device-width, initial-scale=1"><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous"></head>
+<body style="background: linear-gradient( 90deg, pink, pink, pink, pink, pink, black, black)"><u><h1>Soul Checkers</h1></u>
+<table><tr><td><section style="border: solid 4px; width: 30px; height: 30px; background: deeppink; opacity: 75%"><main id="id1" onclick="tile(this)" style="background: red; width: 22px; height: 22px; border-radius: 45px 45px 45px 45px; border: solid 2px black"></main></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: black; opacity: 60%"></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: deeppink; opacity: 75%"><main id="id2" onclick="tile(this)" style="background: red; width: 22px; height: 22px; border-radius: 45px 45px 45px 45px; border: solid 2px black"></main></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: black; opacity: 60%"></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: deeppink; opacity: 75%"><main id="id3" onclick="tile(this)" style="background: red; width: 22px; height: 22px; border-radius: 45px 45px 45px 45px; border: solid 2px black"></main></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: black; opacity: 60%"></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: deeppink; opacity: 75%"><main id="id4" onclick="tile(this)" style="background: red; width: 22px; height: 22px; border-radius: 45px 45px 45px 45px; border: solid 2px black"></main></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: black; opacity: 60%"></section></td></tr>
+<tr><td><section style="border: solid 4px; width: 30px; height: 30px; background: black; opacity: 60%"></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: deeppink; opacity: 75%"><main id="id5" onclick="tile(this)" style="background: red; width: 22px; height: 22px; border-radius: 45px 45px 45px 45px; border: solid 2px black"></main></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: black; opacity: 60%"></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: deeppink; opacity: 75%"><main id="id6" onclick="tile(this)" style="background: red; width: 22px; height: 22px; border-radius: 45px 45px 45px 45px; border: solid 2px black"></main></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: black; opacity: 60%"></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: deeppink; opacity: 75%"><main id="id7" onclick="tile(this)" style="background: red; width: 22px; height: 22px; border-radius: 45px 45px 45px 45px; border: solid 2px black"></main></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: black; opacity: 60%"></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: deeppink; opacity: 75%"><main id="id8" onclick="tile(this)" style="background: red; width: 22px; height: 22px; border-radius: 45px 45px 45px 45px; border: solid 2px black"></main></section></td></tr>
+<tr><td><section style="border: solid 4px; width: 30px; height: 30px; background: deeppink; opacity: 75%"><main id="id9" onclick="tile(this)" style="background: red; width: 22px; height: 22px; border-radius: 45px 45px 45px 45px; border: solid 2px black"></main></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: black; opacity: 60%"></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: deeppink; opacity: 75%"><main id="id10" onclick="tile(this)" style="background: red; width: 22px; height: 22px; border-radius: 45px 45px 45px 45px; border: solid 2px black"></main></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: black; opacity: 60%"></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: deeppink; opacity: 75%"><main id="id11" onclick="tile(this)" style="background: red; width: 22px; height: 22px; border-radius: 45px 45px 45px 45px; border: solid 2px black"></main></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: black; opacity: 60%"></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: deeppink; opacity: 75%"><main id="id12" onclick="tile(this)" style="background: red; width: 22px; height: 22px; border-radius: 45px 45px 45px 45px; border: solid 2px black"></main></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: black; opacity: 60%"></section></td></tr>
+<tr><td><section style="border: solid 4px; width: 30px; height: 30px; background: black; opacity: 60%"></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: deeppink; opacity: 75%"><main id="id13" onclick="tile(this)" style="background: deeppink; width: 22px; height: 22px; border-radius: 45px 45px 45px 45px"></main></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: black; opacity: 60%"></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: deeppink; opacity: 75%"><main id="id14" onclick="tile(this)" style="background: deeppink; width: 22px; height: 22px; border-radius: 45px 45px 45px 45px"></main></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: black; opacity: 60%"></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: deeppink; opacity: 75%"><main id="id15" onclick="tile(this)" style="background: deeppink; width: 22px; height: 22px; border-radius: 45px 45px 45px 45px"></main></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: black; opacity: 60%"></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: deeppink; opacity: 75%"><main id="id16" onclick="tile(this)" style="background: deeppink; width: 22px; height: 22px; border-radius: 45px 45px 45px 45px"></main></section></td></tr>
+<tr><td><section style="border: solid 4px; width: 30px; height: 30px; background: deeppink; opacity: 75%"><main id="id17" onclick="tile(this)" style="background: deeppink; width: 22px; height: 22px; border-radius: 45px 45px 45px 45px"></main></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: black; opacity: 60%"></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: deeppink; opacity: 75%"><main id="id18" onclick="tile(this)" style="background: deeppink; width: 22px; height: 22px; border-radius: 45px 45px 45px 45px"></main></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: black; opacity: 60%"></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: deeppink; opacity: 75%"><main id="id19" onclick="tile(this)" style="background: deeppink; width: 22px; height: 22px; border-radius: 45px 45px 45px 45px"></main></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: black; opacity: 60%"></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: deeppink; opacity: 75%"><main id="id20" onclick="tile(this)" style="background: deeppink; width: 22px; height: 22px; border-radius: 45px 45px 45px 45px"></main></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: black; opacity: 60%"></section></td></tr>
+<tr><td><section style="border: solid 4px; width: 30px; height: 30px; background: black; opacity: 60%"></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: deeppink; opacity: 75%"><main id="id21" onclick="tile(this)" style="background: black; width: 22px; height: 22px; border-radius: 45px 45px 45px 45px"></main></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: black; opacity: 60%"></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: deeppink; opacity: 75%"><main id="id22" onclick="tile(this)" style="background: black; width: 22px; height: 22px; border-radius: 45px 45px 45px 45px"></main></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: black; opacity: 60%"></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: deeppink; opacity: 75%"><main id="id23" onclick="tile(this)" style="background: black; width: 22px; height: 22px; border-radius: 45px 45px 45px 45px"></main></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: black; opacity: 60%"></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: deeppink; opacity: 75%"><main id="id24" onclick="tile(this)" style="background: black; width: 22px; height: 22px; border-radius: 45px 45px 45px 45px"></main></section></td></tr>
+<tr><td><section style="border: solid 4px; width: 30px; height: 30px; background: deeppink; opacity: 75%"><main id="id25" onclick="tile(this)" style="background: black; width: 22px; height: 22px; border-radius: 45px 45px 45px 45px"></main></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: black; opacity: 60%"></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: deeppink; opacity: 75%"><main id="id26" onclick="tile(this)" style="background: black; width: 22px; height: 22px; border-radius: 45px 45px 45px 45px"></main></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: black; opacity: 60%"></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: deeppink; opacity: 75%"><main id="id27" onclick="tile(this)" style="background: black; width: 22px; height: 22px; border-radius: 45px 45px 45px 45px"></main></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: black; opacity: 60%"></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: deeppink; opacity: 75%"><main id="id28" onclick="tile(this)" style="background: black; width: 22px; height: 22px; border-radius: 45px 45px 45px 45px"></main></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: black; opacity: 60%"></section></td></tr>
+<tr><td><section style="border: solid 4px; width: 30px; height: 30px; background: black; opacity: 60%"></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: deeppink; opacity: 75%"><main id="id29" onclick="tile(this)" style="background: black; width: 22px; height: 22px; border-radius: 45px 45px 45px 45px"></main></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: black; opacity: 60%"></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: deeppink; opacity: 75%"><main id="id30" onclick="tile(this)" style="background: black; width: 22px; height: 22px; border-radius: 45px 45px 45px 45px"></main></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: black; opacity: 60%"></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: deeppink; opacity: 75%"><main id="id31" onclick="tile(this)" style="background: black; width: 22px; height: 22px; border-radius: 45px 45px 45px 45px"></main></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: black; opacity: 60%"></section></td>
+<td><section style="border: solid 4px; width: 30px; height: 30px; background: deeppink; opacity: 75%"><main id="id32" onclick="tile(this)" style="background: black; width: 22px; height: 22px; border-radius: 45px 45px 45px 45px"></main></section></td></tr></table>
+<br>
+<table>
+<tr>
+<td><footer style="background:green" id="black"><p onclick="one()">Black</p></footer></td>
+<td><footer style="background:green" id="red"><p onclick="two()">Red</p></footer></td>
+<td><footer style="background:green" id="purple"><p onclick="four()">Purple</p></footer></td>
+<td><footer style="background:green" id="orange"><p onclick="three()">Orange</p></footer></td>
+</tr>
+</table>
+<p>Crafted By <span style="font-family: cursive">Dylan Smith</span></p><button onclick="reset()" class="btn btn-danger">Reset</button>
+<script src="/checkers/new.js"></script>
+</body></html>
