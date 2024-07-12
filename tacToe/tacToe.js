39c88d4b753cac183f65e9d465265ffcb1933cd4
Soulbow3 <138614183+Soulbow3@users.noreply.github.com>
2024-07-11 20:49:30 -0500

    Update tacToe.js

--- tacToe/tacToe.js
+++ tacToe/tacToe.js
@@ -4,15 +4,14 @@ const btn=document.getElementById("btn1");
 function func(element) {
 if (text.innerHTML==="Player X") {
 element.style.background="teal";
-counter.style.background="teal";
+counter.style.background="purple";
 text.innerHTML="Player O";
 element.innerHTML="X";
 btn.style.background="hotpink";
-btn.innerHTML="Turn Toggle"
-}
+btn.innerHTML="Turn Toggle"}
 else if (text.innerHTML==="Player O") {
 element.style.background="purple";
-counter.style.background="purple";
+counter.style.background="teal";
 text.innerHTML="Player X";
 element.innerHTML="O";
 btn.style.background="hotpink";
