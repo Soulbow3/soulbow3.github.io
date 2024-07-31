ac99b5fa926890b579f72989d9833888d999ffb4
Soulbow3 <138614183+Soulbow3@users.noreply.github.com>
2024-07-31 16:01:05 -0500

    Update soulBowsCrypt.js

--- soulBowsCrypt/soulBowsCrypt.js
+++ soulBowsCrypt/soulBowsCrypt.js
@@ -6579,6 +6579,125 @@ go3.innerHTML="Yellow";}
 else if (tile3c.innerHTML==="Pink" && accept.innerHTML==="Ad- vance To Go") {
 tile3c.innerHTML="";
 go3.innerHTML="Pink";}
+
+if (tile1a.innerHTML==="One" && accept.innerHTML==="Jail") {
+tile1a.innerHTML="";
+Jail1.innerHTML="One";}
+else if (tile1a.innerHTML==="Red" && accept.innerHTML==="Jail") {
+tile1a.innerHTML="";
+Jail1.innerHTML="Red";}
+else if (tile1a.innerHTML==="Yellow" && accept.innerHTML==="Jail") {
+tile1a.innerHTML="";
+Jail1.innerHTML="Yellow";}
+else if (tile1a.innerHTML==="Pink" && accept.innerHTML==="Jail") {
+tile1a.innerHTML="";
+Jail1.innerHTML="Pink";}
+
+else if (tile1a.innerHTML==="Two" && accept.innerHTML==="Jail") {
+tile2a.innerHTML="";
+Jail2.innerHTML="Two";}
+else if (tile2a.innerHTML==="Red" && accept.innerHTML==="Jail") {
+tile2a.innerHTML="";
+Jail2.innerHTML="Red";}
+else if (tile2a.innerHTML==="Yellow" && accept.innerHTML==="Jail") {
+tile2a.innerHTML="";
+Jail2.innerHTML="Yellow";}
+else if (tile2a.innerHTML==="Pink" && accept.innerHTML==="Jail") {
+tile2a.innerHTML="";
+Jail2.innerHTML="Pink";}
+
+else if (tile3a.innerHTML==="Three" && accept.innerHTML==="Jail") {
+tile3a.innerHTML="";
+Jail3.innerHTML="Three";}
+else if (tile3a.innerHTML==="Red" && accept.innerHTML==="Jail") {
+tile3a.innerHTML="";
+Jail3.innerHTML="Red";}
+else if (tile3a.innerHTML==="Yellow" && accept.innerHTML==="Jail") {
+tile3a.innerHTML="";
+Jail3.innerHTML="Yellow";}
+else if (tile3a.innerHTML==="Pink" && accept.innerHTML==="Jail") {
+tile3a.innerHTML="";
+Jail3.innerHTML="Pink";}
+
+else if (tile1b.innerHTML==="One" && accept.innerHTML==="Jail") {
+tile1b.innerHTML="";
+Jail1.innerHTML="One";}
+else if (tile1b.innerHTML==="Red" && accept.innerHTML==="Jail") {
+tile1b.innerHTML="";
+Jail1.innerHTML="Red";}
+else if (tile1b.innerHTML==="Yellow" && accept.innerHTML==="Jail") {
+tile1b.innerHTML="";
+Jail1.innerHTML="Yellow";}
+else if (tile1b.innerHTML==="Pink" && accept.innerHTML==="Jail") {
+tile1b.innerHTML="";
+Jail1.innerHTML="Pink";}
+ 
+else if (tile2b.innerHTML==="Two" && accept.innerHTML==="Jail") {
+tile2b.innerHTML="";
+Jail2.innerHTML="Two";}
+else if (tile2b.innerHTML==="Red" && accept.innerHTML==="Jail") {
+tile2b.innerHTML="";
+Jail2.innerHTML="Red";}
+else if (tile2b.innerHTML==="Yellow" && accept.innerHTML==="Jail") {
+tile2b.innerHTML="";
+Jail2.innerHTML="Yellow";}
+else if (tile2b.innerHTML==="Pink" && accept.innerHTML==="Jail") {
+tile2b.innerHTML="";
+Jail2.innerHTML="Pink";}
+ 
+else if (tile3b.innerHTML==="One" && accept.innerHTML==="Jail") {
+tile3b.innerHTML="";
+Jail3.innerHTML="One";}
+else if (tile3b.innerHTML==="Red" && accept.innerHTML==="Jail") {
+tile3b.innerHTML="";
+Jail3.innerHTML="Red";}
+else if (tile3b.innerHTML==="Yellow" && accept.innerHTML==="Jail") {
+tile3b.innerHTML="";
+Jail3.innerHTML="Yellow";}
+else if (tile3b.innerHTML==="Pink" && accept.innerHTML==="Jail") {
+tile3b.innerHTML="";
+Jail3.innerHTML="Pink";}
+
+else if (tile1c.innerHTML==="One" && accept.innerHTML==="Jail") {
+tile1c.innerHTML="";
+Jail1.innerHTML="One";}
+else if (tile1c.innerHTML==="Red" && accept.innerHTML==="Jail") {
+tile1c.innerHTML="";
+Jail1.innerHTML="Red";}
+else if (tile1c.innerHTML==="Yellow" && accept.innerHTML==="Jail") {
+tile1c.innerHTML="";
+Jail1.innerHTML="Yellow";}
+else if (tile1c.innerHTML==="Pink" && accept.innerHTML==="Jail") {
+tile1c.innerHTML="";
+Jail1.innerHTML="Pink";}
+ 
+else if (tile2c.innerHTML==="Three" && accept.innerHTML==="Jail") {
+tile2c.innerHTML="";
+Jail2.innerHTML="Three";}
+else if (tile2c.innerHTML==="Red" && accept.innerHTML==="Jail") {
+tile2c.innerHTML="";
+Jail2.innerHTML="Red";}
+else if (tile2c.innerHTML==="Yellow" && accept.innerHTML==="Jail") {
+tile2c.innerHTML="";
+Jail2.innerHTML="Yellow";}
+else if (tile2c.innerHTML==="Pink" && accept.innerHTML==="Jail") {
+tile2c.innerHTML="";
+Jail2.innerHTML="Pink";}
+
+else if (tile3c.innerHTML==="Three" && accept.innerHTML==="Jail") {
+tile3c.innerHTML="";
+Jail3.innerHTML="Three";}
+else if (tile3c.innerHTML==="Red" && accept.innerHTML==="Jail") {
+tile3c.innerHTML="";
+Jail3.innerHTML="Red";}
+else if (tile3c.innerHTML==="Yellow" && accept.innerHTML==="Jail") {
+tile3c.innerHTML="";
+Jail3.innerHTML="Yellow";}
+else if (tile3c.innerHTML==="Pink" && accept.innerHTML==="Jail") {
+tile3c.innerHTML="";
+Jail3.innerHTML="Pink";}
+
+ 
  
 else if (element.innerHTML==="Red") {
 element.innerHTML="Yellow";}
