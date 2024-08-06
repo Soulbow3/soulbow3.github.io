ef82739c7ccb23724ad8abc560c74d3f543e51a7
Soulbow3 <138614183+Soulbow3@users.noreply.github.com>
2024-08-06 15:36:56 -0500

    Create toggles.js

---
+++ soulBowsCrypt/toggles.js
@@ -0,0 +1,38 @@
+function change1(element) {
+if (element.style.background==="silver") {
+element.style.background="grey";
+element.innerHTML="Sell";}
+else if (element.style.background==="grey") {
+element.style.background="black";
+element.innerHTML="Place";}
+else if (element.style.background==="black") {
+element.style.background="white";
+element.innerHTML="Re- move";}
+else {element.style.background="silver";
+element.innerHTML="Toggle P1 Off";}}
+
+function change2(element) {
+if (element.style.background==="silver") {
+element.style.background="grey";
+element.innerHTML="Sell";}
+else if (element.style.background==="grey") {
+element.style.background="black";
+element.innerHTML="Place";}
+else if (element.style.background==="black") {
+element.style.background="white";
+element.innerHTML="Re- move";}
+else {element.style.background="silver";
+element.innerHTML="Toggle P2 Off";}}
+
+function change3(element) {
+if (element.style.background==="silver") {
+element.style.background="grey";
+element.innerHTML="Sell";}
+else if (element.style.background==="grey") {
+element.style.background="black";
+element.innerHTML="Place";}
+else if (element.style.background==="black") {
+element.style.background="white";
+element.innerHTML="Re- move";}
+else {element.style.background="silver";
+element.innerHTML="Toggle P3 Off";}}
