d87248b22ae9a9aced589abd7b89e4215ead0cf7
Soulbow3 <138614183+Soulbow3@users.noreply.github.com>
2024-08-11 03:07:55 -0500

    Update master.js

--- soulBowsCrypt/master.js
+++ soulBowsCrypt/master.js
@@ -33,10 +33,10 @@ else if (back.style.background==="red") {
 back.style.background="green";}
 else if (back.style.background==="green") {
 back.style.background="red";}
-else if (one.innerHTML==="One") {
+else if (one.innerHTML==="One" && back.style.background==="linear-gradient(180deg, yellow, green, yellow)") {
 three.innerHTML="x30";
 back.style.background="red";}
-else if (four.innerHTML==="Three") {
+else if (four.innerHTML==="Three" && back.style.background==="linear-gradient(180deg, yellow, green, yellow)") {
 three.innerHTML="x30";
 back.style.background="green";}}
 
@@ -75,10 +75,10 @@ else if (back.style.background==="red") {
 back.style.background="green";}
 else if (back.style.background==="green") {
 back.style.background="red";}
-else if (one.innerHTML==="One") {
+else if (one.innerHTML==="One" && back.style.background==="linear-gradient(180deg, yellow, green, yellow)") {
 three.innerHTML="x40";
 back.style.background="red";}
-else if (four.innerHTML==="Three") {
+else if (four.innerHTML==="Three" && back.style.background==="linear-gradient(180deg, yellow, green, yellow)") {
 three.innerHTML="x40";
 back.style.background="green";}}
 
