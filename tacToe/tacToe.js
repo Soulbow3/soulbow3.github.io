0e74a0b288112d4d5204ace4b714cfa1f1f12479
Soulbow3 <138614183+Soulbow3@users.noreply.github.com>
2024-08-18 22:43:10 -0500

    Update tacToe.js

--- tacToe/tacToe.js
+++ tacToe/tacToe.js
@@ -17,3 +17,117 @@ element.innerHTML="O";
 btn.style.background="hotpink";
 btn.innerHTML="Turn Toggle";}
 else if (element.style.background==="hotpink") {}}
+
+function sqaureOne() {
+if (two.style.background==="teal" && three.style.background==="teal") {
+winnerTxt.innerHTML="X Wins";}
+else if (five.style.background==="teal" && nine.style.background==="teal") {
+winnerTxt.innerHTML="X Wins";}
+else if (four.style.background==="teal" && seven.style.background==="teal") {
+winnerTxt.innerHTML="X Wins";}
+else if (two.style.background==="plum" && three.style.background==="plum") {
+winnerTxt.innerHTML="O Wins";}
+else if (five.style.background==="plum" && nine.style.background==="plum") {
+winnerTxt.innerHTML="O Wins";}
+else if (four.style.background==="plum" && seven.style.background==="plum") {
+winnerTxt.innerHTML="O Wins";}}
+
+function sqaureTwo() {
+if (five.style.background==="teal" && eight.style.background==="teal") {
+winnerTxt.innerHTML="X Wins";}
+else if (one.style.background==="teal" && three.style.background==="teal") {
+winnerTxt.innerHTML="X Wins";}
+else if (five.style.background==="plum" && eight.style.background==="plum") {
+winnerTxt.innerHTML="O Wins";}
+else if (one.style.background==="plum" && three.style.background==="plum") {
+winnerTxt.innerHTML="O Wins";}}
+
+function sqaureThree() {
+if (one.style.background==="teal" && two.style.background==="teal") {
+winnerTxt.innerHTML="X Wins";}
+else if (seven.style.background==="teal" && five.style.background==="teal") {
+winnerTxt.innerHTML="X Wins";}
+else if (six.style.background==="teal" && nine.style.background==="teal") {
+winnerTxt.innerHTML="X Wins";}
+else if (one.style.background==="plum" && two.style.background==="plum") {
+winnerTxt.innerHTML="O Wins";}
+else if (five.style.background==="plum" && seven.style.background==="plum") {
+winnerTxt.innerHTML="O Wins";}
+else if (nine.style.background==="plum" && six.style.background==="plum") {
+winnerTxt.innerHTML="O Wins";}}
+
+function sqaureFour() {
+if (one.style.background==="teal" && seven.style.background==="teal") {
+winnerTxt.innerHTML="X Wins";}
+else if (five.style.background==="teal" && six.style.background==="teal") {
+winnerTxt.innerHTML="X Wins";}
+else if (one.style.background==="plum" && seven.style.background==="plum") {
+winnerTxt.innerHTML="O Wins";}
+else if (five.style.background==="plum" && six.style.background==="plum") {
+winnerTxt.innerHTML="O Wins";}}
+
+function middle() {
+if (one.style.background==="teal" && nine.style.background==="teal") {
+winnerTxt.innerHTML="X Wins";}
+else if (three.style.background==="teal" && seven.style.background==="teal") {
+winnerTxt.innerHTML="X Wins";}
+else if (four.style.background==="teal" && six.style.background==="teal") {
+winnerTxt.innerHTML="X Wins";}
+else if (two.style.background==="teal" && eight.style.background==="teal") {
+winnerTxt.innerHTML="X Wins";}
+if (one.style.background==="plum" && nine.style.background==="plum") {
+winnerTxt.innerHTML="O Wins";}
+else if (three.style.background==="plum" && seven.style.background==="plum") {
+winnerTxt.innerHTML="O Wins";}
+else if (four.style.background==="plum" && six.style.background==="plum") {
+winnerTxt.innerHTML="O Wins";}
+else if (two.style.background==="plum" && eight.style.background==="plum") {
+winnerTxt.innerHTML="O Wins";}}
+
+function sqaureSix() {
+if (three.style.background==="teal" && nine.style.background==="teal") {
+winnerTxt.innerHTML="X Wins";}
+else if (five.style.background==="teal" && four.style.background==="teal") {
+winnerTxt.innerHTML="X Wins";}
+else if (three.style.background==="plum" && nine.style.background==="plum") {
+winnerTxt.innerHTML="O Wins";}
+else if (five.style.background==="plum" && four.style.background==="plum") {
+winnerTxt.innerHTML="O Wins";}}
+
+function sqaureSeven() {
+if (four.style.background==="teal" && one.style.background==="teal") {
+winnerTxt.innerHTML="X Wins";}
+else if (five.style.background==="teal" && seven.style.background==="teal") {
+winnerTxt.innerHTML="X Wins";}
+else if (eight.style.background==="teal" && nine.style.background==="teal") {
+winnerTxt.innerHTML="X Wins";}
+else if (four.style.background==="plum" && one.style.background==="plum") {
+winnerTxt.innerHTML="O Wins";}
+else if (five.style.background==="plum" && seven.style.background==="plum") {
+winnerTxt.innerHTML="O Wins";}
+else if (eight.style.background==="plum" && nine.style.background==="plum") {
+winnerTxt.innerHTML="O Wins";}}
+
+function sqaureEight() {
+if (seven.style.background==="teal" && nine.style.background==="teal") {
+winnerTxt.innerHTML="X Wins";}
+else if (five.style.background==="teal" && two.style.background==="teal") {
+winnerTxt.innerHTML="X Wins";}
+else if (seven.style.background==="plum" && nine.style.background==="plum") {
+winnerTxt.innerHTML="O Wins";}
+else if (five.style.background==="plum" && two.style.background==="plum") {
+winnerTxt.innerHTML="O Wins";}}
+
+function sqaureNine() {
+if (eight.style.background==="teal" && seven.style.background==="teal") {
+winnerTxt.innerHTML="X Wins";}
+else if (five.style.background==="teal" && one.style.background==="teal") {
+winnerTxt.innerHTML="X Wins";}
+else if (three.style.background==="teal" && four.style.background==="teal") {
+winnerTxt.innerHTML="X Wins";}
+else if (seven.style.background==="plum" && eight.style.background==="plum") {
+winnerTxt.innerHTML="O Wins";}
+else if (five.style.background==="plum" && one.style.background==="plum") {
+winnerTxt.innerHTML="O Wins";}
+else if (three.style.background==="plum" && four.style.background==="plum") {
+winnerTxt.innerHTML="O Wins";}}
